ecb42ed1d8e55ec446260e9b99e6a0c08eb6c6e8
Alex <dayalex6@gmail.com>
2016-03-29 11:13:48 -0600

    still working on the layouts, just a commit to get access at school

--- app/src/main/java/cs314_a4/adventuregame/AdventureActivity.java
+++ app/src/main/java/cs314_a4/adventuregame/AdventureActivity.java
@@ -41,49 +41,86 @@ public class AdventureActivity extends Activity {
  	public void myClickHandler(View view) {
         TextView myView = (TextView) findViewById(R.id.roomView);
         TextView myItems = (TextView) findViewById(R.id.myItems);
-        TextView myMove = (TextView) findViewById(R.id.myMove);
-        String move;
+        String actionResult = "";
 
         switch (view.getId()) {
             case R.id.goUp:
-                move = model.goUp();
-                myView.setText(model.getView());
-                myMove.setText(move);
+                actionResult = model.goUp();
                 break;
             case R.id.goDown:
-                move = model.goDown();
-                myView.setText(model.getView());
-                myMove.setText(move);
+                actionResult = model.goDown();
                 break;
             case R.id.goNorth:
-                move = model.goNorth();
-                myView.setText(model.getView());
-                myMove.setText(move);
+                actionResult = model.goNorth();
                 break;
             case R.id.goSouth:
-                move = model.goSouth();
-                myView.setText(model.getView());
-                myMove.setText(move);
+                actionResult = model.goSouth();
                 break;
             case R.id.goEast:
-                move = model.goEast();
-                myView.setText(model.getView());
-                myMove.setText(move);
+                actionResult = model.goEast();
                 break;
             case R.id.goWest:
-                move = model.goWest();
-                myView.setText(model.getView());
-                myMove.setText(move);
+                actionResult = model.goWest();
                 break;
             case R.id.drop:
-
                 myItems.setText(model.showItems());
                 break;
             case R.id.grab:
-
                 myItems.setText(model.showItems());
                 break;
         }
+        displayCurrentInfo(actionResult);
+    }
+
+    // private methods
+    // updates info displayed in GUI any time a button is pushed
+    private void displayCurrentInfo(String result){
+        TextView myView = (TextView) findViewById(R.id.roomView);
+        myView.setText(model.getView() + '\n' + result);
+
+        TextView myItems = (TextView) findViewById(R.id.myItems);
+        myItems.setText(model.showItems());
+
+        updateRoomItems();
+        updateUserItems();
+    }
+
+    //updates room items comboBox to reflect what's currently in the room
+    @SuppressWarnings("unchecked")
+    private void updateRoomItems(){
+        roomItems.removeAllItems();
+        Item[] itemList = model.getRoomContents();
+        for(int i = 0; i < itemList.length; i++)
+            roomItems.addItem(itemList[i].getDesc());
+    }
+
+    //updates user items comboBox to reflect what's currently in the user's inventory
+    @SuppressWarnings("unchecked")
+    private void updateUserItems(){
+        userItems.removeAllItems();
+        Item[] itemList = model.getPlayerItems();
+        int numItems = model.getPlayerNumOfItemsCarried();
+        for(int i = 0; i < numItems; i++)
+            userItems.addItem(itemList[i].getDesc());
+    }
+
+
+    private String grab() {
+        //get items currently in room and which item the user selected
+        Item[] itemList = model.getRoomContents();
+        int itemNum = roomItems.getSelectedIndex();
+
+        //check if room has anything in it to grab
+        //itemNum == -1 if room is empty
+        if(itemNum < 0) return "The room is empty.";
+        else return model.grabItem(itemList[itemNum]);
+    }
+
+    private String drop() {
+        //  combobox index because dropItem takes an int for item array index
+        //  add 1 to account for 1-based indexing
+        return model.dropItem(userItems.getSelectedIndex()+1);
+
     }
 
 }
