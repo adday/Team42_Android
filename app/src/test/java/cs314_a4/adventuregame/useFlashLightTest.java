a68aa34a18d06f3645e03e2369fb25cc9877ce49
thewrathofmath <okvathe@gmail.com>
2016-04-18 13:17:31 -0600

    useFlashLightTest added

---
+++ app/src/test/java/cs314_a4/adventuregame/useFlashLightTest.java
@@ -0,0 +1,40 @@
+package cs314_a4.adventuregame;
+
+import org.junit.Test;
+
+import java.lang.Integer;
+
+import cs314_A3.AdventureGameModelFacade;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class UseFlashLightTest {
+    @Test
+    public void useFlashLight_isCorrect() throws Exception {
+        AdventureGameModelFacade AGMF = new AdventureGameModelFacade(1);
+
+        assertEquals(new ArrayList<Integer>, AGMF.useFlashLight());
+
+        AGMF.goEast();
+        AGMF.goDown();
+        AGMF.goEast();
+        AGMF.goNorth();
+        AGMF.goEast();
+
+        AGMF.grabItem(AGMF.getRoomContents()[0]);
+
+        // This is what useFlashLight should noew return in r7
+        ArrayList<Integer> testKey = new ArrayList<Integer>();
+        testKey.add(0);
+        testKey.add(2);
+        testKey.add(1);
+        testKey.add(1);
+        testKey.add(0);
+        testKey.add(0);
+
+        assertEquals(testKey, AGMF.useFlashLight());
+    }
+}
