02a62d3190ec9da6503c2c44be497649b1815ad7
laidbackarkie <aqarkie@rams.colostate.edu>
2016-04-18 12:15:47 -0600

    updated player pickup of loaded items

--- lib/src/main/java/cs314_A3/AdventureGameModelFacade.java
+++ lib/src/main/java/cs314_A3/AdventureGameModelFacade.java
@@ -56,9 +56,10 @@ public class AdventureGameModelFacade {
    */
   private void playerPickUpInitialItems(ArrayList<Integer> savedGameState){
       for(int i=2;i<savedGameState.size();i++) {
-          for(int j = 0; j < getRoomContents().length;j++){
-              if(getRoomContents()[j].getItemId() == -1){
-                  thePlayer.pickUp(getRoomContents()[j]);
+          if(savedGameState.get(i) == -1) {
+              for(int j=0;j<getRoomContents().length;j++){
+                  if(getRoomContents()[j].getItemId() == (i-2))
+                      thePlayer.pickUp(getRoomContents()[j]);
               }
           }
       }
