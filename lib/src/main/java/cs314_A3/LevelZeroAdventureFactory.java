8c585a8fc094410e347e7aeae8af0881ecc189db
Adrion Q Arkenberg <laidbackarkie@yahoo.com>
2016-04-18 22:24:56 -0600

    made winningItem var private as per rubric spec

--- lib/src/main/java/cs314_A3/LevelZeroAdventureFactory.java
+++ lib/src/main/java/cs314_A3/LevelZeroAdventureFactory.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
          Ed Okvath
  **/
 public class LevelZeroAdventureFactory implements AdventureFactory {
-    Item winningItem;
+    private Item winningItem;
 
     @Override
     public Room createAdventure() {
